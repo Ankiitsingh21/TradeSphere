e43f7862dc1de37fd6a50c6a921d5a19cd13777b
Ankit Singh <ankiitsingh21@gmail.com>
2026-03-29 13:11:14 +0530

    primsa setup done in wallet service

--- wallet/src/config/db.ts
+++ wallet/src/config/db.ts
@@ -1,15 +1,23 @@
-import mongoose from "mongoose";
+import {PrismaClient} from "../generated/prisma/client"
+
+const prisma = new PrismaClient({
+  datasources: {
+    url: process.env.DATABASE_URL,
+  },
+});
 
 export const connectDB = async () => {
   const MAX_RETRIES = 5;
 
   for (let i = 1; i <= MAX_RETRIES; i++) {
     try {
-      await mongoose.connect(process.env.MONGO_URI!);
-      console.log("MongoDB Connected");
+      await prisma.$connect();
+
+
+      console.log("PostgreSQL Connected");
       return;
     } catch (error) {
-      console.log(`Mongo connection failed. Attempt ${i}`, error);
+      console.log(`Postgres connection failed. Attempt ${i}`, error);
 
       if (i === MAX_RETRIES) {
         throw error;
--- wallet/src/index.ts
+++ wallet/src/index.ts
@@ -5,11 +5,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
-  if (!process.env.MONGO_URI) {
-    throw new Error("incorrect mongo url");
+  if (!process.env.DATABASE_URL) {
+    throw new Error("incorrect database url");
   }
 
   await connectDB();
+
 
   app.listen(3000, () => {
     console.log(`Listening on 3000`);
