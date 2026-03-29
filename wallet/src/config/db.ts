9effa54dad82792dce6a9438de8de9c80a070d8f
Ankit Singh <ankiitsingh21@gmail.com>
2026-03-29 13:53:47 +0530

    model setup done of postgres

--- wallet/src/config/db.ts
+++ wallet/src/config/db.ts
@@ -1,10 +1,11 @@
-import {PrismaClient} from "../generated/prisma/client"
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient({
-  datasources: {
-    url: process.env.DATABASE_URL,
-  },
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
 });
+
+export const prisma = new PrismaClient({ adapter });
 
 export const connectDB = async () => {
   const MAX_RETRIES = 5;
@@ -12,17 +13,11 @@ export const connectDB = async () => {
   for (let i = 1; i <= MAX_RETRIES; i++) {
     try {
       await prisma.$connect();
-
-
       console.log("PostgreSQL Connected");
       return;
     } catch (error) {
       console.log(`Postgres connection failed. Attempt ${i}`, error);
-
-      if (i === MAX_RETRIES) {
-        throw error;
-      }
-
+      if (i === MAX_RETRIES) throw error;
       await new Promise((res) => setTimeout(res, 5000));
     }
   }
