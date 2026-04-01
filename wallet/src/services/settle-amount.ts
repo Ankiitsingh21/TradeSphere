d5f5d541b5a3e7e87ea3703c56077ab161a9f13c
Ankit Singh <ankiitsingh21@gmail.com>
2026-04-01 10:07:57 +0530

    ingress-issue

--- wallet/src/services/settle-amount.ts
+++ wallet/src/services/settle-amount.ts
@@ -1,13 +1,51 @@
+import { BadRequestError } from "@showsphere/common";
 import { prisma } from "../config/db"
 
 
 
-export const settle = async(userID:string,resolve:number,unresolved:number)=>{
+export const settle = async(userID:string,amount:number)=>{
         const wallet = await prisma.wallet.findUnique({
                 where:{
                         userId:userID
                 }
         });
 
+        if(!wallet){
+                throw new BadRequestError("Wallet not found");
+        }
+
+        
+        const availb= wallet.available_balance.toNumber();
+        const totalb= wallet.total_balance.toNumber();
+        const lockb= wallet.locked_balance.toNumber();
+
+        if(amount>lockb){
+                throw new BadRequestError("loacked balance is inusfficient");
+        }
+
+        const newL = lockb-amount;
+        const newT = availb+newL;
+
+
+        const update = await prisma.wallet.update({
+                where:{
+                        id: wallet.id
+                },data:{
+                        locked_balance:newL,
+                        total_balance:newT
+                }
+        });
+
+
+        const tranc = await prisma.transactions.create({
+                data:{
+                        walletId:wallet.id,
+                        userId:wallet.userId,
+                        amount:amount,
+                        type:"SETTLE"
+                }
+        })
+
+        return update;
         
 }
