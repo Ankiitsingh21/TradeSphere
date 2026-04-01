330848d032c372448d2f3eac121969474782dbbd
Ankit Singh <ankiitsingh21@gmail.com>
2026-04-01 14:16:17 +0530

    setup of natwrapper done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,43 @@
 import { app } from "./app";
 import { connectDB } from "./config/db";
+import { natsWrapper } from "./natswrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
-  if (!process.env.MONGO_URI) {
-    throw new Error("incorrect mongo url");
+  if (!process.env.DATABASE_URL) {
+    throw new Error("incorrect database url");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID is incorrect");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID is incorrect");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL is incorrect");
   }
 
   await connectDB();
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (error) {
+    return ;
+  }
 
   app.listen(3000, () => {
     console.log(`Listening on 3000`);
