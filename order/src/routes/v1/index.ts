d5e9a4bc57d1356d85c69cbb642fe87c252cd3d2
Ankit Singh <ankiitsingh21@gmail.com>
2026-04-02 18:15:35 +0530

    prisma setup done for order service

--- order/src/routes/v1/index.ts
+++ order/src/routes/v1/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-
+router.post('/buy',async(req,res:Response)=>{
+        res.send({});
+})
 
 export default router;
