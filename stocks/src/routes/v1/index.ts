e2f65eaf8daf051e5c49a9e7e20565e7754fc566
Ankit Singh <ankiitsingh21@gmail.com>
2026-04-05 12:59:11 +0530

    stocks service setup done

---
+++ stocks/src/routes/v1/index.ts
@@ -0,0 +1,10 @@
+import { requireAuth, validateRequest } from "@showsphere/common";
+import express from "express";
+import { body } from "express-validator";
+
+
+const router = express.Router();
+
+
+
+export default router;
